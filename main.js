84683a523bf35563e41d14816f6a1fa908031bab
Vito Farenga <vitofarenga@libero.it>
2022-06-15 17:07:20 +0200

    prova 1 nono funzionante

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 // età
-let userAge = prompt("inserisci la tua età");
-console.log(userAge);
 
-document.getElementById("user-age").innerHTML = userAge;
+
+// document.getElementById("user-age").innerHTML = userAge;
 
 // distanza da percorrere
-let userDistance = prompt("inserisci la distanza da percorrere");
-console.log(userDistance);
 
-document.getElementById("user-distance").innerHTML = userDistance;
+
+// document.getElementById("user-distance").innerHTML = userDistance;
 
 // calcolo
-let userPrice = userDistance * 0.21;
+// let userPrice = userDistance * 0.21;
+// const scontoUno = (userPrice * 20) / 100;
+// const scontoDue = (userPrice * 40) / 100;
+// if ( userAge < 18 ) {
+//      userPrice = userPrice - scontoUno;
+// }
+// else if ( userAge > 65 ) {
+//      userPrice = userPrice - scontoDue;
+// }
+
+// document.getElementById("user-price").innerHTML = userPrice.toFixed(2) + "£";
+
+// calcolo
+const button = document.querySelector("only-button")
+const output = document.querySelector("user-price")
 const scontoUno = (userPrice * 20) / 100;
 const scontoDue = (userPrice * 40) / 100;
-if ( userAge < 18 ) {
-     userPrice = userPrice - scontoUno;
-}
-else if ( userAge > 65 ) {
-     userPrice = userPrice - scontoDue;
-}
 
-document.getElementById("user-price").innerHTML = userPrice.toFixed(2) + "£";
+button.addEventListener("click", function(){
+    document.getElementById("user-age").innerHTML = userAge;
+    document.getElementById("user-distance").innerHTML = userDistance;
+    let userPrice = userDistance * 0.21;
+    if ( userAge < 18 ) {
+        userPrice = userPrice - scontoUno;
+   }
+   else if ( userAge > 65 ) {
+        userPrice = userPrice - scontoDue;
+   }
+   
+   output.innerHTML = userPrice.toFixed(2) + "£";
+});
