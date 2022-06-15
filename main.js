f4ae0507fcd14474e0b47e51fd4b7d3c68d2d6ff
Vito Farenga <vitofarenga@libero.it>
2022-06-15 17:23:34 +0200

    milestone-1

--- main.js
+++ main.js
@@ -1,36 +1,16 @@
-// età
+// calcolatore
+const button = document.querySelector("#only-button")
+const output = document.querySelector("#user-price")
 
-
-// document.getElementById("user-age").innerHTML = userAge;
-
-// distanza da percorrere
-
-
-// document.getElementById("user-distance").innerHTML = userDistance;
-
-// calcolo
-// let userPrice = userDistance * 0.21;
-// const scontoUno = (userPrice * 20) / 100;
-// const scontoDue = (userPrice * 40) / 100;
-// if ( userAge < 18 ) {
-//      userPrice = userPrice - scontoUno;
-// }
-// else if ( userAge > 65 ) {
-//      userPrice = userPrice - scontoDue;
-// }
-
-// document.getElementById("user-price").innerHTML = userPrice.toFixed(2) + "£";
-
-// calcolo
-const button = document.querySelector("only-button")
-const output = document.querySelector("user-price")
-const scontoUno = (userPrice * 20) / 100;
-const scontoDue = (userPrice * 40) / 100;
 
 button.addEventListener("click", function(){
-    document.getElementById("user-age").innerHTML = userAge;
-    document.getElementById("user-distance").innerHTML = userDistance;
+    let userAge = document.getElementById("user-age").value;
+    let userDistance = document.getElementById("user-distance").value;
+    
+
     let userPrice = userDistance * 0.21;
+    const scontoUno = (userPrice * 20) / 100;
+    const scontoDue = (userPrice * 40) / 100;
     if ( userAge < 18 ) {
         userPrice = userPrice - scontoUno;
    }
@@ -38,5 +18,5 @@ button.addEventListener("click", function(){
         userPrice = userPrice - scontoDue;
    }
    
-   output.innerHTML = userPrice.toFixed(2) + "£";
+  console.log( userPrice.toFixed(2) + "£");
 });
